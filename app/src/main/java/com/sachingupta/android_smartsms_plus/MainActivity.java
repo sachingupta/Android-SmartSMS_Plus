38e8904920ac3842e35c8fc838cc536d944b34fe
sachingupta <hapysachin91@gmail.com>
2015-11-28 19:27:25 -0800

    Initial commit for Android Smart Message App

---
+++ app/src/main/java/com/sachingupta/android_smartsms_plus/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sachingupta.android_smartsms_plus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
